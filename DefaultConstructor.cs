4697026784106cdba7b13c7e64738cdbf10239ec
Preetam-Kamble <150223054+Preetam-Kamble@users.noreply.github.com>
2024-07-18 21:55:31 +0530

    Add files via upload

---
+++ DefaultConstructor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class DefaultConstructor
+    {
+        int num;
+        DefaultConstructor() // default constructor
+        {
+            num = 10;
+        }
+
+        public void show()
+        {
+            Console.WriteLine(num);
+        }
+
+        public static void Main(string[] args)
+        {
+            DefaultConstructor p = new DefaultConstructor();
+            p.show();
+        }
+    }
+}
