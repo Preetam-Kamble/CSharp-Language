15d812d122b296ad067e2053dd659d6be554527c
Preetam-Kamble <150223054+Preetam-Kamble@users.noreply.github.com>
2024-07-18 21:47:30 +0530

    Add files via upload

---
+++ ContinueBreak.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContinueBreakEx
+{
+    internal class Program
+    {
+        public static void BreakEx()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if(i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i); // Here execution will stop once i reaches at 5
+            }
+        }
+
+        public static void ContinueEx()
+        {
+            for(int i=1; i <= 10; i++)
+            {
+                if(i == 5 || i == 7)
+                {
+                    continue;
+                }
+                Console.WriteLine(i); // Here execute all values except 5 and 7
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            BreakEx(); // O/P - 1 2 3 4
+            Console.WriteLine("---------------------");
+            ContinueEx(); // O/P - 1 2 3 4 6 8 9 10
+        }
+    }
+}
