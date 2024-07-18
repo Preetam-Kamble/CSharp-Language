79b7925f1ff54f4c847782879735b72c72eda867
Preetam-Kamble <150223054+Preetam-Kamble@users.noreply.github.com>
2024-07-18 21:43:08 +0530

    Add files via upload

---
+++ Sum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pro1
+{
+    internal class Sum
+    {
+        
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 30;
+            int sum = a + b;
+            Console.WriteLine(sum);
+        }
+    }
+}
