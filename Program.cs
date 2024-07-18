ae3831c885c19e14024e8ca6c1b946c4fea62f76
Preetam-Kamble <150223054+Preetam-Kamble@users.noreply.github.com>
2024-07-18 21:20:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        int num;
+        Program() // default constructor
+        {
+            num = 10;
+        }
+
+        public void show()
+        {
+            Console.WriteLine(num);
+        }
+
+        public static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.show();
+        }
+    }
+}
