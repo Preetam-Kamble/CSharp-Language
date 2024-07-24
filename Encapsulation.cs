48849d33f6dc62c4e1b9085af44945afc6212162
Preetam-Kamble <150223054+Preetam-Kamble@users.noreply.github.com>
2024-07-24 11:59:53 +0530

    Add files via upload

---
+++ Encapsulation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Encapsulation
+    {
+        private string studentName;
+        private int studentAge;
+
+        public string name
+        {
+            get { return studentName; }
+            set { studentName = value; }
+        }
+
+        public int age
+        {
+            get { return studentAge; }
+            set { studentAge = value; }
+        }
+    }
+
+    class DriverEncp
+    {
+        static public void Main(string[] args)
+        {
+            Encapsulation encp = new Encapsulation();
+            encp.name = "Pritam";
+            encp.age = 22;
+            Console.WriteLine("Name :" + encp.name);
+            Console.WriteLine("Age :" + encp.age);
+        }
+    }
+}
